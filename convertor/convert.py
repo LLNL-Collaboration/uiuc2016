b56159fa00c4fe0945fe2088757e6c77bd346ddf
Huck Zou <framejoe0000@gmail.com>
2016-11-13 16:48:28 -0600

    basic python convertor to convert testmesh.json to Blueprint JSON

---
+++ convertor/convert.py
@@ -0,0 +1,49 @@
+import json
+
+with open("testmesh.json") as file:
+	data = json.load(file)
+
+#Build Blueprint JSON object
+blueprint_dict = {}
+#Build coordsets
+blueprint_dict["coordsets"] = {}
+blueprint_dict["coordsets"]["coords"] = {}
+blueprint_dict["coordsets"]["coords"]["type"] = "explicit"
+blueprint_dict["coordsets"]["coords"]["system"] = "rz"
+blueprint_dict["coordsets"]["coords"]["path"] = "coordsets/coords"
+blueprint_dict["coordsets"]["coords"]["values"] = {"r":[], "z":[]}
+
+#Build topologies
+blueprint_dict["topologies"] = {}
+blueprint_dict["topologies"]["mesh"] = {}
+blueprint_dict["topologies"]["mesh"]["type"] = "unstructured"
+blueprint_dict["topologies"]["mesh"]["coordset"] = "coords"
+blueprint_dict["topologies"]["mesh"]["path"] = "topologies/mesh"
+blueprint_dict["topologies"]["mesh"]["elements"] = {}
+blueprint_dict["topologies"]["mesh"]["elements"]["shape"] = "quad"
+blueprint_dict["topologies"]["mesh"]["elements"]["connectivity"] = []
+
+#Collect r and z from testmesh.json
+r = []
+z = []
+ordered_data = {}
+#Convert key from string to integer
+for key, value in data["coord"].items():
+	ordered_data[int(key)] = value 
+#Loop through ordered data
+for key, value in ordered_data.items():
+	r.append(value["pos"]["r"])
+	z.append(value["pos"]["z"])
+
+blueprint_dict["coordsets"]["coords"]["values"]["r"] = r
+blueprint_dict["coordsets"]["coords"]["values"]["z"] = z
+
+#Collect connectivities
+c = []
+for key, value in data["zones"].items():
+	c.extend(value["nids"])
+
+blueprint_dict["topologies"]["mesh"]["elements"]["connectivity"] = c 
+
+with open("blueprint_mesh.json","wt") as out:
+	json.dump(blueprint_dict, out, indent=4)
