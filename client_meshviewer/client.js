4172d749da87cddec0bc03d5d493e7d1de15a569
Huck Zou <framejoe0000@gmail.com>
2017-02-08 17:54:39 -0600

    meshviewer client

---
+++ client_meshviewer/client.js
@@ -0,0 +1,40 @@
+function websocket(connection, data)
+{
+    // var wsproto = (location.protocol === 'https:') ? 'wss:' : 'ws:';
+    // connection = new WebSocket(wsproto + '//' + window.location.host + '/websocket');
+    connection = new WebSocket("ws://localhost:9000/mesh_phpserver")
+    connection.onopen = function (event) {
+        $("#status_display").html("<font color=green>[status=success]socket connection</font>");
+        connection.send("testmesh.json");
+    }
+    connection.onmessage = function (msg) 
+    {
+        // var data;
+        try
+        {
+            data=JSON.parse(msg.data);
+            $("#status_display").html("<font color=green>[status=success]</font>");
+        }
+        catch(e)
+        {
+             //caught an error in the above code, simply display to the status element
+            $("#status_display").html("<font color=red>[status=error] " + e + "</font>");
+        }
+    }
+      
+    connection.onerror = function (error)
+    {
+        console.log('WebSocket error');
+        console.log(error)
+        connection.close();
+        $("#status_display").html("<font color=red>[status=error]</font>");
+    }
+    
+    connection.onclose = function (error)
+    {
+        console.log('WebSocket closed');
+        console.log(error)
+        connection.close();
+        $("#status_display").html("<font color=orange>[status=disconnected]</font>");
+    }
+}
